13d10930a31ad21f462594c9cda420a139f3abfe
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-07-25 11:36:48 -0300

    embeded js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
 function showSlides(n) {
   let i;
@@ -16,7 +20,9 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
- 
+  dots[slideIndex-1].className += " active";
 }
